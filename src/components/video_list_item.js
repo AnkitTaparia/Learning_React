ccb7f3ca6908ccf81fc1a4c9c1b58c1270eac983
root <root@localhost.localdomain>
2018-09-30 00:36:11 +0530

    Final commit (app completed)

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const VideoListItem = ({videos}) => {
+const VideoListItem = ({videos,onVideoSelect}) => {
     const imageUrl= videos.snippet.thumbnails.default.url;
     return(
-        <li className='list-group-item'>
+        <li onClick={()=> onVideoSelect(videos)} className='list-group-item'>
             <div className='video-list media'>
                 <div className='media-left'>
                     <img className='media-object'src={imageUrl} />
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -12,17 +13,26 @@ const API_KEY='AIzaSyD1010RUFfOYkNGEUyF-0rINt-ccHiBs6k';
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state={videos :[]};
-
-    YTSearch({key:API_KEY , term:'surfboards'} ,(videos) => {
-      this.setState({videos}); 
+    this.state={
+      videos :[],
+      selectedVideo : null
+    };
+  this.videoSearch('learning react');  
+  }
+  videoSearch(term){
+    YTSearch({key:API_KEY , term:term} ,(videos) => {
+      this.setState({videos :videos, selectedVideo: videos[0]}); 
     });
   }
   render(){
+  const videoSearch=_.debounce((term) => {this.videoSearch(term)},300 )  
   return (<div>
-              <SearchBar />
-              <VideoDetail videos={this.state.videos[0]}/>
-              <VideoList videos = { this.state.videos} />
+              <SearchBar onVideoSearchChange={videoSearch}/>
+              <VideoDetail videos={this.state.selectedVideo}/>
+              <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos = { this.state.videos} 
+              />
           </div>
          );
   }
