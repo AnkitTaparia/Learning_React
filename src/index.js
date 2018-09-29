ec54c8a448f8e7f5e6fe6aebb7275c7fe3addfd8
root <root@localhost.localdomain>
2018-09-29 19:24:47 +0530

    second commit (Adding components)

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY='AIzaSyD1010RUFfOYkNGEUyF-0rINt-ccHiBs6k';
+
+
 // First component (it produces html)
-const App = () => {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={videos :[]};
+
+    YTSearch({key:API_KEY , term:'surfboards'} ,(videos) => {
+      this.setState({videos}); 
+    });
+  }
+  render(){
   return (<div>
               <SearchBar />
-          </div>);
+              <VideoList videos = { this.state.videos} />
+          </div>
+         );
+  }
 }
 
 // Displaying in DOM
